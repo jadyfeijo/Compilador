7d5ed58231b0ede5d24e8ef5542de435b0691122
egtrindade <egtrindade@inf.ufrgs.br>
2019-03-21 19:09:14 -0300

    Added hash struct

---
+++ hash.c
@@ -0,0 +1,29 @@
+///Eduarda Trindade 274709
+//Jady Feijó 230210
+
+#include "hash.h"
+
+void hashInit(void)
+{
+    //do something
+}
+
+int hashAddress(char *text)
+{
+    //do something
+}
+
+NODE* hashFind(char *text)
+{
+    //do something
+}
+
+NODE* hashInsert(int type, char *text)
+{
+    //do something
+}
+
+void hashPrint(void)
+{
+    //do something
+}
---
+++ hash.h
@@ -0,0 +1,21 @@
+///Eduarda Trindade 274709
+//Jady Feijó 230210
+
+#include <stdio.h>
+
+#define HASH_SIZE 997
+
+typedef struct hash_node
+{
+    int type;
+    char *text;
+    struct hash_node *next;      
+} NODE;
+
+NODE *table[HASH_SIZE];
+
+void hashInit(void);
+int hashAddress(char *text);
+NODE* hashFind(char *text);
+NODE* hashInsert(int type, char *text);
+void hashPrint(void);
