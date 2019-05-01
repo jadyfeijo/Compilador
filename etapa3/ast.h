d665ef1f02691a44b77033dc46a8c5c2d50b3740
jadyfeijo <42940544+jadyfeijo@users.noreply.github.com>
2019-05-01 01:11:24 -0300

    Added others nodes

--- etapa3/ast.h
+++ etapa3/ast.h
@@ -1,6 +1,8 @@
 //Eduarda Trindade 274709
 //Jady Feijó 230210
 
+#ifndef AST_HEADER
+#define AST_HEADER
 #include <stdio.h>
 #include "hash.h"
 
@@ -28,12 +30,30 @@
 #define AST_READ 20
 #define AST_RETURN 21
 #define AST_LCMD 22
-#define AST_FUNC 23
-#define AST_DEC 24
+#define AST_DECFUNC 23
+#define AST_VARDEC 24
 #define AST_ASSIGN 25
 #define AST_ARRAY 26
 #define AST_BLOCK 27
 #define AST_PARENTH 28
+#define AST_DECFUNC_VOID 29
+#define AST_VETDEC 30
+#define AST_DEC 31
+#define AST_DEC_PARAM 32
+#define AST_DEC_PARAM2 33
+#define AST_PRINT_PARAM 34
+#define AST_LITFLOAT 35
+#define AST_INT 36
+#define AST_FLOAT 37
+#define AST_BYTE 38
+#define AST_ARRAY_INIT 39
+#define AST_ARRAY_INIT2 40
+#define AST_FUNCCALL 41
+#define AST_FUNC_PARAM 42
+#define AST_FUNC_PARAM2 43
+
+
+
 
 typedef struct ast_node
 {
@@ -42,6 +62,7 @@ typedef struct ast_node
     struct ast_node *son[MAX_SONS];
 } AST;
 
-AST *astCreate(int type, AST *s0, AST *s1, AST *s2, AST *s3);
+AST *astCreate(int type, NODE *symbol,AST *s0, AST *s1, AST *s2, AST *s3);
 void astPrint(int level, AST *node);
-void astDecompilation(AST *node);
+
+#endif
