f85b84d1574445dbf16dee22f0c682ef7e1e4be8
jadyfeijo <42940544+jadyfeijo@users.noreply.github.com>
2019-05-26 16:32:57 -0300

    Added some cases in checkOperands()

--- etapa4/semantic.c
+++ etapa4/semantic.c
@@ -2,12 +2,19 @@
 #include <stdlib.h>
 #include "semantic.h"
 
-//PROTOTYPES
+int semanticError=0;
+AST *nodeDeclared=NULL;
+
+void functionValidation (AST *node);
+
 
 void setAndCheckRedeclared(AST *node)
 {
     int i;
     AST *aux=NULL;
+
+    nodeDeclared=node;
+
 
     if(node == 0)
         return;
@@ -18,8 +25,10 @@ void setAndCheckRedeclared(AST *node)
     if(node->type == AST_VARDEC || node->type == AST_VETDEC || node->type == AST_DECFUNC_VOID || node->type == AST_DECFUNC)
     {
         if(node->symbol->type != SYMBOL_IDENTIFIER)
+        {
             fprintf(stderr,"SEMANTIC ERROR: Identifier %s redeclared type=%d \n",node->symbol->text,node->symbol->type);
-
+            semanticError=1;
+        }
 
         else
         {
@@ -33,15 +42,18 @@ void setAndCheckRedeclared(AST *node)
                 case AST_DECFUNC:
                 {
                     if(node->son[1]->symbol->type != SYMBOL_IDENTIFIER) 
+                    {
                         fprintf(stderr,"SEMANTIC ERROR: Identifier %s redeclared\n",node->son[1]->symbol->text);
+                        semanticError=1;
+                    }
 
                     else
                     {
                       switch(node->son[1]->son[0]->type) //tipos da lista de parametros
                       {
-                          case AST_INT: node->son[1]->symbol->datatype = SYMBOL_DATATYPE_INT;
-                          case AST_FLOAT: node->son[1]->symbol->datatype = SYMBOL_DATATYPE_FLOAT;
-                          case AST_BYTE: node->son[1]->symbol->datatype = SYMBOL_DATATYPE_BYTE;
+                          case AST_INT: node->son[1]->symbol->datatype = SYMBOL_DATATYPE_INT; break;
+                          case AST_FLOAT: node->son[1]->symbol->datatype = SYMBOL_DATATYPE_FLOAT;break;
+                          case AST_BYTE: node->son[1]->symbol->datatype = SYMBOL_DATATYPE_BYTE;break;
                           default: break;
                       }
                       node->son[1]->symbol->type= SYMBOL_VAR;
@@ -57,15 +69,17 @@ void setAndCheckRedeclared(AST *node)
                             aux=aux->son[1]->son[0];
                             switch (aux->son[0]->type)
                             {
-                                case AST_INT: aux->symbol->datatype = SYMBOL_DATATYPE_INT;
-                                case AST_FLOAT: aux->symbol->datatype = SYMBOL_DATATYPE_FLOAT;
-                                case AST_BYTE: aux->symbol->datatype = SYMBOL_DATATYPE_BYTE;
+                                case AST_INT: aux->symbol->datatype = SYMBOL_DATATYPE_INT; break;
+                                case AST_FLOAT: aux->symbol->datatype = SYMBOL_DATATYPE_FLOAT; break;
+                                case AST_BYTE: aux->symbol->datatype = SYMBOL_DATATYPE_BYTE; break;
                                 default: break;
                             }
 
                             if (aux->symbol->type !=SYMBOL_IDENTIFIER)
+                            {
                                 fprintf(stderr,"SEMANTIC ERROR: Identifier %s redeclared\n",aux->symbol->text);
-
+                                semanticError=1;
+                            }
                             aux->symbol->type = SYMBOL_VAR;
                             
                         }
@@ -76,16 +90,16 @@ void setAndCheckRedeclared(AST *node)
                     default: break;
             }
         }
-
         switch (node->son[0]->type)
         {
-            case AST_INT: node->symbol->datatype = SYMBOL_DATATYPE_INT;
-            case AST_FLOAT: node->symbol->datatype = SYMBOL_DATATYPE_FLOAT;
-            case AST_BYTE: node->symbol->datatype = SYMBOL_DATATYPE_BYTE;
+            case AST_INT: node->symbol->datatype = SYMBOL_DATATYPE_INT; break;
+            case AST_FLOAT: node->symbol->datatype = SYMBOL_DATATYPE_FLOAT; break;
+            case AST_BYTE: node->symbol->datatype = SYMBOL_DATATYPE_BYTE; break;
             default: break;
         }
 
     }
+
 }
 
 void checkOperands(AST *node)
@@ -115,5 +129,68 @@ void checkOperands(AST *node)
         )
         fprintf(stderr,"Invalid first operand of +\n");
         break;
+
+        case AST_ASSIGN:
+        if(node->symbol->type !=SYMBOL_VAR)
+            fprintf(stderr,"SEMANTIC ERROR: Symbol %s must be scalar!\n", node->symbol->text);
+            
+        if(node->symbol->datatype !=node->son[0]->symbol->datatype)
+        {
+            if(node->symbol->datatype==SYMBOL_DATATYPE_FLOAT || node->son[0]->symbol->datatype==SYMBOL_DATATYPE_FLOAT)
+            {fprintf(stderr,"SEMANTIC ERROR: Imcompatible type in assign %s = %s \n",node->symbol->text,node->son[0]->symbol->text);
+            semanticError=1;}
+
+        }
+        break;
+
+        case AST_ASSIGNARRAY:
+        if(node->symbol->datatype !=node->son[1]->symbol->datatype)
+        {
+            if(node->symbol->datatype==SYMBOL_DATATYPE_FLOAT || node->son[1]->symbol->datatype==SYMBOL_DATATYPE_FLOAT)
+            {fprintf(stderr,"SEMANTIC ERROR: Imcompatible type in assign %s[%s] = %s",node->symbol->text,node->son[0]->symbol->text,node->son[1]->symbol->text);
+            semanticError=1;
+            break;
+            }
+
+        }
+        if(node->son[0]->symbol->datatype!=SYMBOL_DATATYPE_INT && node->son[0]->symbol->datatype!=SYMBOL_DATATYPE_BYTE)
+        {
+            fprintf(stderr,"SEMANTIC ERROR: Invalid Index type in assign %s[%s] = %s\n",node->symbol->text,node->son[0]->symbol->text,node->son[1]->symbol->text);
+            semanticError=1;
+        }
+        break;
+
+        case AST_FUNCCALL:
+                  fprintf(stderr,"Passou na func %s \n",node->symbol->text);
+
+        if(node->symbol->type !=SYMBOL_FUN)
+        {
+            fprintf(stderr,"SEMANTIC ERROR: Identifier %s is not a function\n",node->symbol->text);
+            semanticError=1;
+
+        }
+      //  functionValidation(node);
+       /* else
+        {
+            switch (functionValidation(node))
+            {
+            
+            
+            default:
+                break;
+            }
+        }*/
+        
+break;
+
+default:break;
+
     }
 }
+
+/*void functionValidation (AST *node)
+{
+
+    
+
+}*/
